993483634a92f1a6934e309f2d23f6b0dbf1ad20
Nathan <nathan.lauga@gmail.com>
2021-04-11 17:50:09 +0200

    update plot function

--- dyslexia/plots/plots.py
+++ dyslexia/plots/plots.py
@@ -25,7 +25,7 @@ def plot_image(img: np.ndarray, figsize=(10, 10), gray=True):
     plt.show()
 
 
-def plot_2_images(img1: np.ndarray, img2: np.ndarray, figsize=(18, 9)):
+def plot_2_images(img1: np.ndarray, img2: np.ndarray, figsize=(18, 9), gray=(False, True)):
     """Plots original image and the processed one
 
     Parameters
@@ -36,16 +36,22 @@ def plot_2_images(img1: np.ndarray, img2: np.ndarray, figsize=(18, 9)):
         Preprocessed image
     figsize : tuple, default (18, 9)
         Figure size
+    gray: tuple(bool), default (False, True)
+        Whether it's a grayscale image or not
     """
 
     fig, axes = plt.subplots(1, 2, figsize=figsize)
 
     ax = axes[0]
-    ax.imshow(img1)
-    ax.set_title('Original')
+    if gray[0]:
+        ax.imshow(img1, cmap='gray')
+    else:
+        ax.imshow(img1)
 
     ax = axes[1]
-    ax.imshow(img2, cmap='gray')
-    ax.set_title('Preprocessed')
+    if gray[1]:
+        ax.imshow(img2, cmap='gray')
+    else:
+        ax.imshow(img2)
 
     plt.show()
