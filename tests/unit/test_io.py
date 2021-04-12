8bbedf51d6b04dc6808631bf0dbbdc1d9d9db3f2
Nathan <nathan.lauga@gmail.com>
2021-04-12 18:56:40 +0200

    update test_io without crop parameter

--- tests/unit/test_io.py
+++ tests/unit/test_io.py
@@ -8,14 +8,14 @@ test_path = Path(__file__).resolve().parents[1]
 def test_load_image_type():
     image_path = test_path / "data" / "images" / "Sample_0.jpeg"
 
-    image = io.load_image(str(image_path), crop_border=False)
-
+    image = io.load_image(str(image_path))
+    
     assert isinstance(image, np.ndarray)
 
 
 def test_load_image_size():
     image_path = test_path / "data" / "images" / "Sample_0.jpeg"
 
-    image = io.load_image(str(image_path), crop_border=False)
-
+    image = io.load_image(str(image_path))
+    
     assert image.shape == (2607, 1834, 3)
