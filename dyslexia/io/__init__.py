398749ddc0537ed9db6b19604fd6cc2d04933ca6
Nathan <nathan.lauga@gmail.com>
2021-06-24 23:18:35 +0200

    Update: read from pdf

--- dyslexia/io/__init__.py
+++ dyslexia/io/__init__.py
@@ -3,3 +3,4 @@ __all__ = ['load_image', 'load_image_from_url', 'load_image_from_string']
 from .load_image import load_image
 from .load_image import load_image_from_url
 from .load_image import load_image_from_string
+from .load_image import load_image_pdf
